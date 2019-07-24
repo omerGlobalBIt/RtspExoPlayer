cf7acf1e8801aae1e500677978ccf71ca6638b4e
omerGlobalBit <omer@globalbit.co.il>
2019-07-24 16:46:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/omer/rtsp_exoplayer/ExampleClass.java
@@ -0,0 +1,12 @@
+package com.example.omer.rtsp_exoplayer;
+
+import android.net.rtp.RtpStream;
+
+import com.google.android.exoplayer2.SimpleExoPlayer;
+
+public class ExampleClass {
+
+    public void exampleTest(){
+        SimpleExoPlayer player;
+    }
+}
