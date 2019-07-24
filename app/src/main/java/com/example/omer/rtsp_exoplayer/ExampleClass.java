87e66218bdb079d23d7af57ba93d3d0c4c63dc7d
omerGlobalBit <omer@globalbit.co.il>
2019-07-24 17:12:32 +0300

    change exoplayer path

--- app/src/main/java/com/example/omer/rtsp_exoplayer/ExampleClass.java
+++ app/src/main/java/com/example/omer/rtsp_exoplayer/ExampleClass.java
@@ -2,11 +2,10 @@ package com.example.omer.rtsp_exoplayer;
 
 import android.net.rtp.RtpStream;
 
-import com.google.android.exoplayer2.SimpleExoPlayer;
 
 public class ExampleClass {
 
     public void exampleTest(){
-        SimpleExoPlayer player;
+
     }
 }
